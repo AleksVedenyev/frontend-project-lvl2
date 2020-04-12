ac44b449e0829a8f955e424b562c14a56c5f4f42
aleksvedenyev <aleks.vedenev@gmail.com>
2020-04-13 00:01:00 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+import { version, parse } from 'commander';
 
-program
-    .version('1.0.0')
+version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<firstConfig> <secondConfig>')
 
-    program.parse(process.argv);
+    parse(process.argv);
