d8eabb1956fcddbbe93ff7c6fd39b1acdbd50d04
aleksvedenyev <aleks.vedenev@gmail.com>
2020-05-19 11:27:53 +0300

    remove babel, install Eslint, update files

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import program from '../commander';
+import program from '../main.js';
 
 program();
--- src/index.js
+++ src/index.js
@@ -9,23 +9,23 @@ const getDiff = (filepath1, filepath2) => {
   const diffirenceOfBefore = keysBefore.reduce((acc, key) => {
     if (keysAfter.includes(key)) {
       if (getDataFromFirstJson[key] === getDataFromSecondJson[key]) {
-        acc.push(`    ${key}: ${getDataFromSecondJson[key]}`)
+        acc.push(`    ${key}: ${getDataFromSecondJson[key]}`);
       } else {
-        acc.push([`  + ${key}: ${getDataFromSecondJson[key]}`])
-        acc.push([`  - ${key}: ${getDataFromFirstJson[key]}`])
+        acc.push([`  + ${key}: ${getDataFromSecondJson[key]}`]);
+        acc.push([`  - ${key}: ${getDataFromFirstJson[key]}`]);
       }
     } else {
-      acc.push([`  - ${key}: ${getDataFromFirstJson[key]}`])
+      acc.push([`  - ${key}: ${getDataFromFirstJson[key]}`]);
     }
     return acc;
-  }, [])
+  }, []);
   const diffirenceOfAfter = keysAfter.reduce((acc, key) => {
     if (!keysBefore.includes(key)) {
       acc.push([`  + ${key}: ${getDataFromSecondJson[key]}`]);
     }
     return acc;
-    }, diffirenceOfBefore)
-  return console.log(`{\n` + diffirenceOfAfter.join('\n') + `\n}`);
-}
+  }, diffirenceOfBefore);
+  return console.log(`{\n${diffirenceOfAfter.join('\n')}\n}`);
+};
 
 export default getDiff;
