a8a895d1580466668e135a6f95fb26950fd79b75
aleksvedenyev <aleks.vedenev@gmail.com>
2020-11-22 23:58:58 +0300

    add some fix in Index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import getParsedData from './parsers.js';
 
 const getDiff = (filepath1, filepath2) => {
-  const getDataFromFirstPath = getParsedData(filepath1);
-  const getDataFromSecondPath = getParsedData(filepath2);
-  const keysOfFirstFile = Object.keys(getDataFromFirstPath);
-  const keysOfSecondFile = Object.keys(getDataFromSecondPath);
+  const dataFromFirstPath = getParsedData(filepath1);
+  const dataFromSecondPath = getParsedData(filepath2);
+  const keysOfFirstFile = Object.keys(dataFromFirstPath);
+  const keysOfSecondFile = Object.keys(dataFromSecondPath);
   const getDiffInFirstFile = keysOfFirstFile.reduce((acc, key) => {
     if (keysOfSecondFile.includes(key)) {
-      if (getDataFromFirstPath[key] === getDataFromSecondPath[key]) {
-        acc.push(`    ${key}: ${getDataFromSecondPath[key]}`);
+      if (dataFromFirstPath[key] === dataFromSecondPath[key]) {
+        acc.push(`    ${key}: ${dataFromSecondPath[key]}`);
       } else {
-        acc.push([`  + ${key}: ${getDataFromSecondPath[key]}`]);
-        acc.push([`  - ${key}: ${getDataFromFirstPath[key]}`]);
+        acc.push([`  + ${key}: ${dataFromSecondPath[key]}`]);
+        acc.push([`  - ${key}: ${dataFromFirstPath[key]}`]);
       }
     } else {
-      acc.push([`  - ${key}: ${getDataFromFirstPath[key]}`]);
+      acc.push([`  - ${key}: ${dataFromFirstPath[key]}`]);
     }
     return acc;
   }, []);
   const getDiffInSecondFile = keysOfSecondFile.reduce((acc, key) => {
     if (!keysOfFirstFile.includes(key)) {
-      acc.push([`  + ${key}: ${getDataFromSecondPath[key]}`]);
+      acc.push([`  + ${key}: ${dataFromSecondPath[key]}`]);
     }
     return acc;
   }, getDiffInFirstFile);
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import getDiff from './index.js';
 const programm = program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(getDiff(filepath1, filepath2));
