bc5a4d3ce33c9d980ed1a83dd36a538edce1d339
aleksvedenyev <aleks.vedenev@gmail.com>
2020-11-25 00:22:26 +0300

    updated tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,19 +1,17 @@
+/* eslint-disable no-underscore-dangle */
 
 import path from 'path';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
 import getDiff from '../src/index.js';
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', __dirname);
-const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('getDiff', () => {
-  expect(getDiff(readFile('file1.json'), readFile('file2.json'))).toEqual(
+  expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(
     `{
       common: {
         + follow: false
