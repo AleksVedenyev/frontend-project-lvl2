5a9bc3fee3da5ee7d8f846052ce14ed4839399a5
aleksvedenyev <aleks.vedenev@gmail.com>
2020-12-05 17:48:14 +0300

    upd json.js

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,26 +1,3 @@
-/* const render = (diff) => diff.flatMap((element) => {
-  const {
-    key, value, type, children,
-  } = element;
-  if (type === 'added') {
-    return JSON.stringify({ Key: key, value, type });
-  }
-  if (type === 'deleted') {
-    return JSON.stringify({ key, value, type });
-  }
-  if (type === 'unchanged') {
-    return JSON.stringify({ key, value, type });
-  }
-  if (type === 'changed') {
-    return JSON.stringify({ key, oldValue: value.oldValue, newValue: value.newValue, type });
-  }
-  if (type === 'embedded') {
-    return JSON.stringify({
-      key, children: render(children), type,
-    });
-  }
-}); */
-
 const diffJSON = (diff) => JSON.stringify(diff);
 
 export default diffJSON;
