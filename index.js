b27fb353fdd98860997663397b1e3223d3d1aafe
aleksvedenyev <aleks.vedenev@gmail.com>
2020-12-09 21:24:54 +0300

    made fixes with naming of package and move index.js to upper lvl

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import getParsedData from './parsers.js';
-import getDiff from './getDiff.js';
-import stylish from './formatters/stylish.js';
-import formatSelector from './formatters/index.js';
+import getParsedData from './src/parsers.js';
+import getDiff from './src/getDiff.js';
+import stylish from './src/formatters/stylish.js';
+import formatSelector from './src/formatters/index.js';
 
 const result = (filepath1, filepath2, formatName = stylish) => {
   const dataFromFirstPath = getParsedData(filepath1);
