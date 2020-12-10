4b7d4cdd0f54a96a1d8d7b47c625ee1f9590abbf
aleksvedenyev <aleks.vedenev@gmail.com>
2020-12-10 13:36:53 +0300

    add _.sortBy() in getDiff module

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 const getDiff = (obj1, obj2) => {
-  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const keys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
   const result = (arr) => arr.map((key) => {
     const oldValue = obj1[key];
     const newValue = obj2[key];
